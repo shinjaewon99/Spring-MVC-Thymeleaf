49917b8801ab0f45840f39a7b361358081659e79
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-02 00:10:42 +0900

    refactor : thymeleaf 기본기능에 대한 매핑

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -76,6 +76,73 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model){
+
+        addUsers(model);
+        return "basic/each";
+    }
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "basic/condition";
+    }
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+
+    }
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("신재원" , 25));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("신재원", 25));
+        list.add(new User("홍길동", 30));
+        list.add(new User("아무개", 13));
+
+        model.addAttribute("users", list);
+
+
+    }
+
     @Data
     // 내부에서 사용 static
     static class User{
